50d0971d73d45cc66e621e8c5cd2a10972efb85f
Peter Halachan <peter.halachan@gmail.com>
2016-03-09 22:41:31 +0100

    form check is working

--- js/script.js
+++ js/script.js
@@ -1,9 +1,35 @@
 $(function() {
-	//alert('hello');
 	$('body').hide();
 	$('body').fadeIn("4000");
 
+	$('#error-message').hide();
+	$('#error-mail').hide();
+
 	$('a').click(function() {
 		$('body').fadeOut('4000');
+	});
+
+	$('form').submit(function() {
+		var email = $('#inputEmail'),
+			message = $('#inputMessage');
+
+		var valid = true;
+
+		//$(this).find('.error').removeClass('error');
+		//$(this).find('.error-message').hide();
+		$('#error-message').hide().css({'color': '#1aa3ff', 'font-weight': 'bold'});
+		$('#error-mail').hide().css({'color': '#1aa3ff', 'font-weight': 'bold'});
+
+		if (!message.val()) {
+			message.addClass('error').next().fadeIn('fast');
+			valid = false;
+		};
+
+		if (!email.val() || !email.val().match( /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/ ) ) {
+			email.addClass('error').next().fadeIn('fast');
+			valid = false;
+		};
+
+		return valid;
 	});
 });
